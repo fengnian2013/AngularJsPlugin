066abe2a870f5db27f905ab78dcfc927c77b6288
fengnian2013 <742703639@qq.com>
2016-06-24 10:44:17 +0800

    新建

---
+++ AngularPlunin/AngularPlunin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularPlunin.Startup))]
+namespace AngularPlunin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
